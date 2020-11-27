15c3164212125914c695498cef2348d79666e8a3
Shaunters <tpimentel368@gmail.com>
2020-11-27 18:04:36 -0300

    Adicionado suporte ao Instagram + bug fixes

--- README.md
+++ README.md
@@ -1,10 +1,12 @@
-# GetFollowed
+# 💻 GetFollowed 💻
 Script feito com o intuito de ganhar seguidores por meio de receber follows mútuos.
 
 ## Redes Sociais suportadas (até então):
-- Twitter (somente tema padrão);
+- 🐦 Twitter (somente tema padrão);
+- 📷 Instagram
 
-## To-Do:
+### To-Do:
 - [ ] Adicionar suporte à mais redes sociais;
-- [ ] Documentar melhor o código;
+- [ ] Adicionar suporte à outros temas do Twitter;
+- [X] Documentar melhor o código;
 - [ ] Adicionar suporte à inglês;
--- src/main.py
+++ src/main.py
@@ -3,7 +3,7 @@ import colorama
 import os, sys, keyboard
 from time import sleep
 
-ART = colorama.Fore.BLUE + \
+ARTE = colorama.Fore.BLUE + \
 """
 \n\n
    _____      _   ______    _ _                       _ 
@@ -15,7 +15,7 @@ ART = colorama.Fore.BLUE + \
 \n\n
 """
 
-# Selecting the button to click;
+"""Função inicial, que retorna qual botão deverá ser usado"""
 def chooseButton():
     print(colorama.Fore.LIGHTBLUE_EX + "Escolha qual rede social usando os números abaixo:")
     print("1. Twitter;\n2. Instagram;\n")
@@ -27,49 +27,52 @@ def chooseButton():
     elif option == "2":
         path = "{}/imgs/instagram.png".format(sys.argv[0].split("main.py")[0])
     elif option not in ("1", "2"):
-        print(colorama.Back.RED + "\nEscolha inválida;\n")
-        chooseButton()
+        print(colorama.Fore.RED + "\nEscolha inválida;" + colorama.Fore.RESET + "\n")
+        return chooseButton()
 
     return path
 
-# Script main loop;
-def mainloop(currentButton):
-    running = True
-    while running:
-
-        if keyboard.is_pressed("esc"):
-            break
-
-        sleep(1.5)
-        location = findButton(currentButton)
-
-        if location == None:
-            pyautogui.scroll(5)
-            pass
-        else:
-            pyautogui.moveTo(location)
-            pyautogui.click()
-            pass
-
-# Find and click button;
+"""Função que tenta encontrar e retornar o botão"""
 def findButton(currentButton, trust=0.8):
     try:
         return pyautogui.locateOnScreen(currentButton, confidence=trust)
     except pyautogui.ImageNotFoundException:
         return None
 
-# Clear console and print ART;
+"""Loop principal que roda até ser parado usando a tecla ESC"""
+def mainloop(currentButton):
+    while True:
+
+        # Sair se ESC estiver pressionado;
+        if keyboard.is_pressed("esc"):
+            break
+
+        sleep(1.5)
+        # Localizar o botão;
+        location = findButton(currentButton)
+
+        # Se não for encontrado, scrollar pra baixo;
+        if location == None:
+            pyautogui.scroll(-125)
+            pass
+        else:
+            pyautogui.moveTo(location)
+            pyautogui.click()
+            pass
+
+# Limpando o console (ele também verifica se o SO é Windows ou Linux);
 if os.name == "nt":
     os.system("clear")
 else:
     os.system("cls")
 
-print(ART)
+print(ARTE)
 
-# Get button;
+# Chamando a função de escolher o botão;
 currentButton = chooseButton()
 print(colorama.Fore.YELLOW + "\n\nAVISO:" + colorama.Fore.RESET + \
       " Segure a tecla ESC para fechar! \n\nIniciando em 5 SEG!")
+
 sleep(5)
 print(colorama.Fore.GREEN + "\n\nIniciado!\n" + colorama.Fore.RESET)
 mainloop(currentButton)
