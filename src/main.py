04178857dbcab3e359cdd45a64077a2da0dc747c
Shaunters <tpimentel368@gmail.com>
2020-11-28 01:35:49 -0300

    The code is now fully written in english + New exception handler

--- src/main.py
+++ src/main.py
@@ -7,10 +7,10 @@ import pyautogui, colorama
 import os, sys, keyboard
 from time import sleep
 
-# Define se deve logar prints de DEBUGGING;
+# Defines if DEBUG messages appear or not;
 DEBUG = False
 
-# Definindo "shortcuts" para o colorama;
+# Making "shortcuts" to colorama;
 RED = colorama.Fore.RED
 BLUE = colorama.Fore.BLUE
 LBLUE = colorama.Fore.LIGHTBLUE_EX
@@ -18,8 +18,8 @@ YELLOW = colorama.Fore.YELLOW
 GREEN = colorama.Fore.GREEN
 RESET = colorama.Fore.RESET
 
-# Arte ASCII;
-ARTE = BLUE + \
+# ASCII art;
+ART = BLUE + \
 """
 \n\n
    _____      _   ______    _ _                       _ 
@@ -31,46 +31,46 @@ ARTE = BLUE + \
 \n
 """
 
-"""Classe principal, contém todos os métodos do script"""
+"""Main class, contains all script methods and variables."""
 class Main():
-    """Iniciando a classe, define algumas variáveis."""
+    """Initiating the class."""
     def __init__(self, debug=False) -> None:
         self.DEBUG = debug
-        # Caminho para o .png a ser usado;
-        self.caminhoBotao = ""
-        # Float de confiança, usado pelo locateOnScreen();
-        self.confianca = 0.8
-        # Tempo de esperar à cada iteração do mainLoop();
+        # Path to the .png button image;
+        self.buttonPath = ""
+        # Confidence float used by the locateOnScreen();
+        self.confidence = 0.8
+        # Waiting time at each iteration of mainLoop();
         self.waitTime = 0.5
-        # Tamanho do scroll quando o botão não for localizado;
+        # Scroll size when the button was not found;
         self.scrollAmount = -150
 
-        # Dicionários contendo os textos de várias linguagens;
-        self.englishLS = dict({
+        # Language Sheets containing the languages texts;
+        self.englishLS = {
                               "choiceSocial": "Choose the social media to be used: (USE THE NUMBERS)",
                               "choiceError": "\nInvalid choice!\n",
                               "startingWarning": "WARNING -> ",
                               "startingMessage": "The script will start in 5 seconds, open the social media page!\n(Hold ESC to exit!)\n",
                               "startingInit": "Initialized with success!",
-                              "closing":"Closing..."
-                              })
-        self.portuguesLS = dict({
+                              "closing": "\nClosing..."
+                              }
+        self.portuguesLS = {
                               "choiceSocial": "Escolha a rede social à ser usada: (USE OS NÚMEROS)",
                               "choiceError": "\nEscolha inválida!\n",
                               "startingWarning": "AVISO -> ",
                               "startingMessage": "O script irá iniciar em 5 segundos,!\n(Segure ESC para sair!)\n",
                               "startingInit": "Iniciado com sucesso!",
-                              "closing":"Fechando..."
-                              })
-        # Dicionário que irá conter a linguagem usada atualmente;
-        self.actualLS = dict({})
+                              "closing":"\nFechando..."
+                              }
+        # Dictionary that will contain the current Language Sheet;
+        self.actualLS = self.englishLS
 
-    """Função print própria da classe, usada para DEBUGGING."""
+    """Print function made with DEBUG purpouses."""
     def __print(self, texto) -> None:
         if self.DEBUG:
             print(YELLOW + texto)
 
-    """Função para escolher a linguagem e setar nos Language Sheets (LS) corretos."""
+    """Method to choose the language and set the correct Language Sheets."""
     def __chooseLanguage(self) -> None:
         print("\nWhat language do you want to use?\n")
         print("1. English;\n2. Português Brasileiro;\n")
@@ -86,8 +86,8 @@ class Main():
             print("\nInvalid choice!\n")
             return self.__chooseLanguage()
 
-    """Retorna o input do usuário com a rede social escolhida."""
-    def __pegarEscolha(self) -> str:
+    """Returns the input from the user containing the path to the social media button choosed."""
+    def __getPathInput(self) -> str:
         print(LBLUE + self.actualLS["choiceSocial"])
         print("1 - Twitter;\n2 - Instagram;\n")
         
@@ -99,69 +99,66 @@ class Main():
             return "{}/imgs/instagram.png".format(sys.argv[0].split("main.py")[0])
         else:
             print(RESET + RED + self.actualLS["choiceError"] + RESET)
-            return self.__pegarEscolha()
+            return self.__getPathInput()
 
-    """Retorna as coordenadas do botão ou None se não for achado."""
-    def __localizarBotao(self) -> str or None:
+    """Returns the button coordinates, if not found, returns None."""
+    def __locateButton(self) -> str or None:
         try:
-            temp = pyautogui.locateOnScreen(self.caminhoBotao, confidence=self.confianca)
+            temp = pyautogui.locateOnScreen(self.buttonPath, confidence=self.confidence)
             return temp
         except pyautogui.ImageNotFoundException:
             return None
     
-    """Método principal, chamado de fora para iniciar a execução."""
+    """Main method of the class, called from outside."""
     def run(self) -> None:
-        try:
-            os.system("cls" if os.name == "nt" else "clear")
-            self.__chooseLanguage()
-            print(ARTE)
-            self.caminhoBotao = self.__pegarEscolha()
+        
+        os.system("cls" if os.name == "nt" else "clear")
+        self.__chooseLanguage()
+        print(ART)
+        self.buttonPath = self.__getPathInput()
 
-            print("\n\n" + YELLOW + self.actualLS["startingWarning"] + RESET + self.actualLS["startingMessage"])
-            sleep(5)
-            print("\n" + GREEN + self.actualLS["startingInit"] + RESET)
-            if self.DEBUG: print(YELLOW + "(DEBUG MODE)" + RESET)
+        print("\n\n" + YELLOW + self.actualLS["startingWarning"] + RESET + self.actualLS["startingMessage"])
+        sleep(5)
+        print("\n" + GREEN + self.actualLS["startingInit"] + RESET)
+        if self.DEBUG: print(YELLOW + "(DEBUG MODE)" + RESET)
 
-            self.__mainLoop()
-            print(RESET)
-            quit()
+        self.__mainLoop()
+        print(RESET)
+        quit()
 
-        except Exception as e:
-            print(RED + f"ERROR: \n{e}\n\n" + RESET)
-            quit()
-        except KeyboardInterrupt:
-            print(RED + self.actualLS["closing"])
-            print(RESET)
-            return
-
-    """Loop principal, pega a localização e clica."""
+    """Main loop of the class."""
     def __mainLoop(self) -> None:
         while True:
-            self.__print("Rodando o loop;\n")
-            self.__print("Esperando 0.2segs;\n")
+            self.__print("Running the loop;\n")
+            self.__print("Waiting 0.2 seconds;\n")
             sleep(self.waitTime)
 
-            self.__print("Verificando se ESC está pressionado;\n")
+            self.__print("Verifying if ESC is pressed;\n")
             if keyboard.is_pressed("ESC"):
-                self.__print("ESC está pressionado, fechando;\n")
+                self.__print("ESC is pressed, closing;\n")
+                print(RED + self.actualLS["closing"])
                 break
 
-            self.__print("Tentando localizar o botão;\n")
-            coords = self.__localizarBotao()
+            self.__print("Trying to find the button;\n")
+            coords = self.__locateButton()
             if coords is None:
-                self.__print("O botão não localizado, scrollando;\n")
+                self.__print("Button not found, scrolling down;\n")
                 pyautogui.scroll(self.scrollAmount)
                 pass
             else:
-                self.__print("O botão foi localizado, movendo o mouse;\n")
+                self.__print("Button found, moving to it;\n")
                 pyautogui.moveTo(coords)
-                self.__print("O mouse foi movido, clickando;\n")
+                self.__print("The mouse was moved, clicking;\n")
                 pyautogui.click()
                 self.__print("Click!\n")
                 pass
 
 if __name__ == "__main__":
     try:
-        Main(debug=DEBUG).run()
+        mainC = Main(debug=DEBUG)
+        mainC.run()
     except KeyboardInterrupt:
-        pass
+        print(RED + mainC.actualLS["closing"] + RESET)
+    except Exception as e:
+        print(RED + f"ERROR: \n{e}\n\n" + RESET)
+        quit()
